086007a0685e602f72e7f4477e7f51b223f2f689
joweyel <falsh@protonmail.com>
2025-06-08 10:44:34 +0200

    small change

--- 3.5/create_s3_bucket_block.py
+++ 3.5/create_s3_bucket_block.py
@@ -1,28 +1,27 @@
 import os
 from time import sleep
 from prefect_aws import S3Bucket, AwsCredentials
+from pydantic import SecretStr
 
 ## IMPORTANT: Dont hardcode the credentials in the program
 ##            Rather use environment variables.
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 
+
 def create_aws_creds_block():
     my_aws_creds_obj = AwsCredentials(
         aws_access_key_id=AWS_ACCESS_KEY_ID, 
-        aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+        aws_secret_access_key=SecretStr(AWS_SECRET_ACCESS_KEY)
     )
     my_aws_creds_obj.save(name="my-aws-creds", overwrite=True)
-
 
 def create_s3_bucket_block():
     aws_creds = AwsCredentials.load("my-aws-creds")
     my_s3_bucket_obj = S3Bucket(
         bucket_name="zoomcamp-mlops-prefect-bucket", credentials=aws_creds
     )
-    # my_s3_bucket_obj.save(name="s3-bucket-example", overwrite=True)
     my_s3_bucket_obj.save(name="s3-bucket-block", overwrite=True)
-
 
 if __name__ == "__main__":
     create_aws_creds_block()
